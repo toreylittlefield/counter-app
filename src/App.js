4e2af04f3ce2cc75deff95f9ef210fa68e6308df
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-04-24 09:23:09 +0800

    add button and function to decrement count w/ test

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@
 
     const increment = makeIncrementer(1);
 
+    const decrement = makeIncrementer(-1);
+
       return (
         <div>
           <p>Count: {count}</p>
           <button className="increment" onClick={increment}>Increment count</button>
+          <button className="decrement" onClick={decrement}>Decrement count</button>
         </div>
       )
   }
