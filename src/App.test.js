2ed11f8550ec87e96cefcbc6205fbab4fb13e138
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-04-24 09:04:13 +0800

    shallow rendering test with enzyme for init state

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,13 @@
-// import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom';
-import App from './App';
+    // src/App.test.js
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+    import React from 'react';
+    import { shallow } from 'enzyme';
+    import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(5);
-  });
-});
+    describe('App component', () => {
+      it('starts with a count of 0', () => {
+        const wrapper = shallow(<App />);
+        const text = wrapper.find('p').text();
+        expect(text).toEqual('Count: 0');
+      });
+    });
